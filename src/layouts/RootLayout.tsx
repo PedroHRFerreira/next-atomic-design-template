4b2e2e153982f7774088127cd038621721440ce0
PedroHRFerreira <devpedrohr@gmail.com>
2026-01-08 11:23:07 -0300

    feat: :fire: Limpando o RootLayout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,23 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/assets/globals.scss";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <div className={`${geistSans.variable} ${geistMono.variable}`}>
+    <div>
       <header>
         <h1>Header do Site</h1>
       </header>
